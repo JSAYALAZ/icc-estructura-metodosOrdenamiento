50ea32ec344fcca7e9595d02e427881af0d573b5
GalaxyAYL <josealbertoayalazumba@gmail.com>
2024-04-10 16:20:45 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int opc;
+
+        System.out.println("Menu");
+        System.out.println("1) Metodo Burbuja");
+        System.out.println("2) Metodo Insercion");
+        System.out.println("3) Metodo Selecciona");
+        System.out.print("Opcion: ");
+        
+    }
+}
